fc14ac1e8b459427990beb325331e90612ec75ea
Sergei Zaitsev <sz@versta24.ru>
2022-10-06 22:54:05 +0300

    server is done

---
+++ SosnovkaRC.Server/SosnovkaRC.Repository/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using SosnovkaRC.Domain.Models;
+
+namespace SosnovkaRC.Repository.Repositories;
+
+public interface IRepository<T> where T : Entity
+{
+    T? GetById(int id);
+    T[] Get(int skip = 0, int? take = null);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(int id);
+    void Delete(T entity);
+}
+
+public class Repository<T> : IRepository<T> where T : Entity
+{
+    protected readonly SosnovkaContext Context;
+
+    public Repository(SosnovkaContext context)
+    {
+        Context = context;
+    }
+
+    public T? GetById(int id) => Context.Set<T>().FirstOrDefault(e => e.Id == id);
+
+    public T[] Get(int skip = 0, int? take = null)
+    {
+        IQueryable<T> query = Context.Set<T>();
+        if (skip > 0)
+            query = query.Skip(skip);
+
+        if (take > 0)
+            query = query.Take(take.Value);
+        return query.ToArray();
+    }
+
+    public void Add(T entity) => Context.Set<T>().Add(entity);
+
+    public void Update(T entity) => Context.Set<T>().Update(entity);
+
+    public void Delete(int id)
+    {
+        var entity = GetById(id);
+        if (entity != null)
+            Delete(entity);
+    }
+
+    public void Delete(T entity) => Context.Set<T>().Remove(entity);
+}
---
+++ SosnovkaRC.Server/SosnovkaRC.Utils/CSmart.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using SmartFormat;
+using SmartFormat.Core.Settings;
+using SmartFormat.Extensions;
+
+namespace SosnovkaRC.Utils;
+
+/// <summary>
+///     Надстройка над SmartFormat, чтобы везде работали дефолтные настройки
+/// </summary>
+public class CSmart
+{
+    private static readonly SmartFormatter _formatter = CreateCustomFormatter();
+
+    private static SmartFormatter CreateCustomFormatter()
+    {
+        var formatter = Smart.CreateDefaultSmartFormat(new SmartSettings
+        {
+            Formatter = new FormatterSettings { ErrorAction = FormatErrorAction.Ignore },
+            Parser = new ParserSettings { ErrorAction = ParseErrorAction.Ignore }
+        });
+
+        formatter.AddExtensions(new IsMatchFormatter { RegexOptions = RegexOptions.CultureInvariant | RegexOptions.IgnoreCase });
+        return formatter;
+    }
+
+    public static string Format(string format, params object?[] args) => _formatter.Format(format, args);
+}
