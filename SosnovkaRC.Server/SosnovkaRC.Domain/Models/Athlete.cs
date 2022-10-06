0edf704988b03d66a2320095d1eb1bc1f8b82dae
Sergei Zaitsev <sz@versta24.ru>
2022-10-06 23:49:33 +0300

    Athlete model

---
+++ SosnovkaRC.Server/SosnovkaRC.Domain/Models/Athlete.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SosnovkaRC.Domain.Models;
+
+[Table("Athletes")]
+public class Athlete : Entity
+{
+    [MaxLength(100)]
+    [Required]
+    public string FirstName { get; set; } = null!;
+
+    [MaxLength(100)]
+    [Required]
+    public string LastName { get; set; } = null!;
+
+    public DateTime DoB { get; set; }
+    public DateTime JoiningDate { get; set; }
+    public DateTime? LeavingDate { get; set; }
+    public string? Bio { get; set; }
+}
--- SosnovkaRC.Server/SosnovkaRC.Repository/SosnovkaContext.cs
+++ SosnovkaRC.Server/SosnovkaRC.Repository/SosnovkaContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SosnovkaRC.Domain.Models;
 
 namespace SosnovkaRC.Repository;
 
 public class SosnovkaContext : DbContext
 {
+    public DbSet<Athlete> Athletes { get; set; } = null!;
+
     public SosnovkaContext(DbContextOptions options) : base(options)
     {
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
